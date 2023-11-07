e7707d3abdeb978809f722249f117f2ea8a0f016
sosiabera <sosiabera32@gmail.com>
2023-11-07 09:26:20 -0400

    a

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import './index.css'
 import "bootstrap/dist/css/bootstrap.css";
 import AppContext from "./assets/context/AppContext.jsx";
 
