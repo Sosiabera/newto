1f7a7e652556d07859f95cf07de218c21cbff3f8
yanitbee <yanithaftomm@gmail.com>
2023-11-07 06:14:32 -0800

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
+
+   fontFamily:{
+   
+   BebasNeue: ['Bebas Neue' , 'sans-serif'],
+   DancingScript: ['Dancing Script' , 'cursive'],
+   Montserrat: ['Montserrat' , 'sans-serif'],
+   Poppins: ['Poppins' , 'sans-serif'],
+
+   },
+
     extend: {},
   },
   plugins: [],
 }
-
